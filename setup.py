68ab0b9af7c000a96b1ba5ef75c3b83ebc917884
Shriraj Gandhi <shrirajgandhi@gmail.com>
2022-03-07 21:08:10 -0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+   name='international_data_preprocessing',
+   version='1.0',
+   description='A module to help normalize international data based on population, inflation, and foreign exchange rates',
+   author='Emma Carlson',
+   author_email='emcar98@uw.edu',
+   packages=['international_data_preprocessing'],
+   install_requires=['pandas'],
+   license='MIT',
+   url='https://github.com/emcarlson2017/international-data-preprocessing',
+)
